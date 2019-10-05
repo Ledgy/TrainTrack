bbf2fcca01fd0ef330dbfa3c5d4e3e4ecb62ae86
julesair <jules.henze@gmail.com>
2019-10-05 17:02:27 +0200

    add some more types and resolvers

--- src/lambda/api/resolvers.js
+++ src/lambda/api/resolvers.js
@@ -1,10 +1,17 @@
+const sampleLocation = {latitude: 1, longtitude: 1, displayName: "test"};
 module.exports = db => ({
   Query: {
     hello: async (root, args, context) => {
       return "Hello, world!";
     },
     location: async () => {
-      return {latitude: 1, longtitude: 1, displayName: "test"};
+      return sampleLocation;
+    },
+    trip: async () => {
+      return {date: new Date(2012, 11, 24)};
+    },
+    user: async () => {
+      return {userId: "wqdqwwd", name: "spock", city: sampleLocation, country: "CH"}
     }
   }
 });
--- src/lambda/api/typedefs.js
+++ src/lambda/api/typedefs.js
@@ -8,8 +8,24 @@ module.exports = gql`
     displayName: String
   }
 
+  type Trip {
+    userId: ID
+    origin: LocationType
+    destination: LocationType
+    date: String
+  }
+
+  type userProfile {
+    userId: ID
+    name: String
+    city: LocationType
+    country: String
+  }
+
   type Query {
     hello: String
     location: LocationType
+    trip: Trip
+    user: userProfile
   }
 `;

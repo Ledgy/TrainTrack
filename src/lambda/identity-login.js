cd141114de642fabd296d786951a16e87bfb99d5
Marius Colacioiu <marius.colacioiu@gmail.com>
2019-10-06 15:48:19 +0200

    updates identity

--- src/lambda/identity-login.js
+++ src/lambda/identity-login.js
@@ -1,11 +1,13 @@
 const initDbApi = require("./api/db_api");
 
 exports.handler = async function (event, context, callback) {
+  console.log(`event: ${event}`);
   const api = await initDbApi();
   try {
     const body = JSON.parse(event.body);
+    console.log(`id: ${body.user.id}`);
     console.log(`email: ${body.user.email}`);
-    console.log(`name:  ${body.user.user_metadata.name}`);
+    console.log(`name:  ${body.user.user_metadata.full_name}`);
 
     // TODO: upsert new user
     api.createLog({ message: 'upsert new user', body, context });

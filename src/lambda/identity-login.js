e20b0f19ac61a6c8c72f38272d5fa218ee8c2d6a
Marius Colacioiu <marius.colacioiu@gmail.com>
2019-10-06 14:36:33 +0200

    Debug identity login callback

--- src/lambda/identity-login.js
+++ src/lambda/identity-login.js
@@ -1,12 +1,14 @@
-// const connectToMongoDB = require("./api/db");
+const initDbApi = require("./api/db_api");
 
-export function handler(event, context, callback) {
-  const db = null;
+export async function handler(event, context, callback) {
+  const api = await initDbApi();
   const body = JSON.parse(event.body);
   console.log(`email: ${body.user.email}`);
   console.log(`name:  ${body.user.user_metadata.name}`);
 
   // TODO: upsert new user
+
+  console.log('upsert new user', { body, api: !!api });
 
   callback(null, {
     statusCode: 200,

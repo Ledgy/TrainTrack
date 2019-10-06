a84edc6f5eb9631012aefd33f13333c80807062a
Jahlela Hasle <jahlela@ledgy.com>
2019-10-06 15:52:39 +0200

    Small styles

--- src/ui/Pages/Profile.jsx
+++ src/ui/Pages/Profile.jsx
@@ -14,10 +14,10 @@ export const Profile = ({ name }) => (
       </div>
     </div>
     <div className="row">
-      <div className="col col-xs-12 col-md-6">
+      <div className="col col-sm-12 col-lg-6">
         <Trips />
       </div>
-      <div className="col col-xs-12 col-md-6">
+      <div className="col col-sm-12 col-lg-6">
         <h3>Map</h3>
       </div>
     </div>
